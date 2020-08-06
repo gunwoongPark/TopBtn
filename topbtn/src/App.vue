c2af49b7fc2bdf909586e73b3548009c172dc262
gunwoongPark <bung1438@gmail.com>
2020-08-06 10:48:14 +0900

    사이트 최상단으로 이동하는 버튼 구현

---
+++ topbtn/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-content style="height:10000px;">
+        <v-contianer>
+          <v-row>
+            <v-col cols="10"></v-col>
+            <v-col cols="2">
+              <h1>Hello World!</h1>
+            </v-col>
+          </v-row>
+        </v-contianer>
+      </v-content>
+    </v-main>
+    <top-btn />
+  </v-app>
+</template>
+
+<script>
+import TopBtn from "./components/TopBtn.vue";
+export default {
+  name: "App",
+
+  components: {
+    TopBtn,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
